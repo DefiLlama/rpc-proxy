f5ba1bf744b4f33f79d70f98403fe1f8b0da9cbc
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-01-17 16:59:52 +0100

    add route

--- chains/kamino.js
+++ chains/kamino.js
@@ -1,6 +1,8 @@
 const { Kamino } = require('@kamino-finance/kliquidity-sdk')
+const { getReservesForMarket, PROGRAM_ID } = require('@kamino-finance/klend-sdk')
 const { Connection, } = require("@solana/web3.js")
 const HyperExpress = require('hyper-express')
+const { PublicKey } = require('@solana/web3.js')
 
 let cachedTvl = null
 let cacheTimestamp = null
@@ -18,11 +20,29 @@ async function getCachedTvl() {
   return cachedTvl
 
   async function getTvl() {
-    const connection = new Connection(process.env.SOLANA_RPC)
+    const connection = getConnection()
     const kamino = new Kamino('mainnet-beta', connection)
     const shareData = await kamino.getStrategiesShareData({});
     return shareData.reduce((a, i) => a + i.shareData.balance.computedHoldings.totalSum.toNumber(), 0)
   }
+}
+
+async function getKaminoLendMarketReserves(market) {
+  market = new PublicKey(market)
+  const  reserves  = await getReservesForMarket(market, getConnection(), PROGRAM_ID)
+  return [...reserves].map(([_, i]) => ({
+    token: i.state.collateral.mintPubkey.toString(),
+    price: Number(i.tokenOraclePrice.price),
+    decimals: i.tokenOraclePrice.decimals.e
+  }))
+}
+
+let connection
+function getConnection() {
+  if (!connection) {
+    connection = new Connection(process.env.SOLANA_RPC)
+  }
+  return connection
 }
 
 function setRoutes(routerPrime) {
@@ -31,6 +51,11 @@ function setRoutes(routerPrime) {
 
   router.get('/tvl', async (_req, res) => {
     res.json(await getCachedTvl())
+  })
+
+  router.post('/lend/market', async (req, res) => {
+    const { market } = req.body
+    res.json(await getKaminoLendMarketReserves(market))
   })
 }
 
